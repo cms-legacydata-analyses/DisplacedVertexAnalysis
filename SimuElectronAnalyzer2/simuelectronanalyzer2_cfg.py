19d0f0ab128766db5ebec57c5c996dffed89f429
Felipe Navarro <felinavarro94@gmail.com>
2019-06-05 13:03:03 -0500

    saving

--- SimuElectronAnalyzer2/simuelectronanalyzer2_cfg.py
+++ SimuElectronAnalyzer2/simuelectronanalyzer2_cfg.py
@@ -23,23 +23,7 @@ process.source = cms.Source("PoolSource",
     # replace 'myfile.root' with the source file you want to use
     duplicateCheckMode = cms.untracked.string('noDuplicateCheck'),
     fileNames = cms.untracked.vstring(
-'file:simu/recosimu10.root',
-'file:simu/recosimu11.root',
-'file:simu/recosimu12.root',
-'file:simu/recosimu13.root',
-'file:simu/recosimu14.root',
-'file:simu/recosimu15.root',
-'file:simu/recosimu16.root',
-'file:simu/recosimu1.root',
-'file:simu/recosimu2.root',
-'file:simu/recosimu3.root',
-'file:simu/recosimu4.root',
-'file:simu/recosimu5.root',
-'file:simu/recosimu6.root',
-'file:simu/recosimu7.root',
-'file:simu/recosimu8.root',
-'file:simu/recosimu9.root'
-
+'root://eospublic.cern.ch//eos/opendata/cms/MonteCarlo2011/Summer11LegDR/DYJetsToLL_TuneZ2_M-50_7TeV-madgraph-tauola/AODSIM/PU_S13_START53_LV6-v1/00000/0A968A44-75BD-E311-8265-0025905A6094.root'
    )
 )
 
